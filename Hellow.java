401bcf3a7af130bbe3d3ff4009ac3d070d3d84ea
pankaj kumar <pk01906@gmail.com>
2023-07-17 14:09:43 +0000

    edited to hellow from local

--- Hellow.java
+++ Hellow.java
@@ -2,6 +2,10 @@ class Hello
 {
 Public static void main(String args[])
 {
+<<<<<<< HEAD
 System.out.println(“hello from remote”);
+=======
+System.out.println(“hello from local”);
+>>>>>>> 5fe2c97 (edited to hellow from local)
 }
 }
